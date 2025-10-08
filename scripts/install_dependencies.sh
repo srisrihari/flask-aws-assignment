779c5a811563b097bc07e4980f4e273f95d4c829
Srihari Kanyaboina <srihari.k.srinivas@gmail.com>
2025-10-08 19:35:59 +0530

    Initial commit

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -e
+
+# Navigate to application directory
 cd /home/ec2-user/flask-app
-sudo yum update -y
-sudo yum install python3 python3-pip -y
-sudo pip3 install -r requirements.txt
-sudo pip3 install gunicorn
+
+# Remove requests if it's causing issues (ignore errors)
+sudo pip3 uninstall -y requests || true
+
+# Upgrade pip
+sudo pip3 install --upgrade pip
+
+# Install dependencies with --ignore-installed flag for problematic packages
+sudo pip3 install --ignore-installed requests
+sudo pip3 install -r requirements.txt --ignore-installed
+
+# Set proper permissions
+sudo chown -R ec2-user:ec2-user /home/ec2-user/flask-app
